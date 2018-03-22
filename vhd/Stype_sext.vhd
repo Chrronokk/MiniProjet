9e0fbfec9e98ac8c093752542a6807692896c272
Fabien METAYER <fabien.metayer@phelma.grenoble-inp.fr>
2018-03-22 11:07:05 +0100

    fin mémoire

--- vhd/Stype_sext.vhd
+++ vhd/Stype_sext.vhd
@@ -3,11 +3,10 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity S_type_sext is
-  
+
   port (
-    input : in  std_logic_vector(31 downto 0)   --code de l'instructionen entrée
-    sign  : in  std_logic                       --définit si l'extention est signed ou non
-    ouput : out std_logic_vector(31 downt)o 0); -- immédiat en sign extended
+    input : in  std_logic_vector(31 downto 0)  --code de l'instructionen entrée
+    ouput : out std_logic_vector(31 downt)o 0);  -- immédiat en sign extended
 
 end entity S_type_sext;
 
@@ -20,15 +19,10 @@ begin  -- architecture A
   -- type   : combinational
   -- inputs : input
   -- outputs: output
-  combi: process (input,sign) is
+  combi : process (input, sign) is
   begin  -- process combi
-    output <= (others => '0');
-    if (sign='1') then
-      output(31) <= input(31);
-      ouput(10 downto 0) <= input(30 downto 25) & input(11 downto 7);
-    else
-      output(11 downto 0) <= input(31 downto 25) & input (11 downto 7);
-    end if;
-    end process combi;
+    output              <= (others => '0');
+    output(11 downto 0) <= input(31 downto 25) & input (11 downto 7);
+  end process combi;
 
 end architecture A;
--- vhd/memory.vhd
+++ vhd/memory.vhd
@@ -4,71 +4,67 @@ use ieee.numeric_std.all;
 
 
 entity memory is
-  
+
   port (
-    read_write : in  std_logic;                      -- 0 pour read 1 pour write
-    adresse    : in  std_logic_vector(31 downto 0)   -- adresse d'accès mémoire
-    data_in    : in  std_logic_vector(31 downto 0;   -- données à charger dans la mémoire
+    mem_access : in  std_logic;         -- utilisation de la mémoire
+    read_write : in  std_logic;         -- 0 pour read 1 pour write
+    adresse    : in  std_logic_vector(31 downto 0)  -- adresse d'accès mémoire
+    data_in    : in  std_logic_vector(31 downto 0);  -- données à charger dans la mémoire
     data_out   : out std_logic_vector(31 downto 0);  -- données à charger dans le registre
-    ex         : in  std_logic_vector(1 downto 0));  -- signal de sélection des extentions de données
-    sign       : in  std_logic;                      -- signal de sélection sur
-                                                     -- le signe
+    size       : in  std_logic_vector(1 downto 0);  -- signal de sélection des extentions de données
+    sign       : in  std_logic);        -- signal de sélection sur le signe
 
-    end entity memory;
+end entity memory;
 
-    architecture A of memory is
+architecture A of memory is
 
-    begin  -- architecture A
+begin  -- architecture A
   type B32 is array (0 to 2048) of std_logic_vector(31 downto 0);  -- mémoire interne
   signal memory : B32;                  -- mémoire
-      -- purpose: gere les accès mémoire
-      -- type   : combinational
-      -- inputs : read_write,data_in,sel_signex
-      -- outputs: data_out
-      comb: process (read_write,data_in,ex,sign) is
-          begin  -- process comb
-        data_out <= (others => '0');        
-        if read_write = '1' then -- lecture
-          if ex = "01" then
-            if sign='0' then
-                  data_out(7 downto 0) <= memory(to_integer(unsiged(adresse))(7 downto 0);
-            else
-                  data_out(31)<= memory(to_integer(unsiged(adresse))(7);
-                  data_out(6 downto 0) <= memory(to_integer(unsiged(adresse))(6 downto 0);
-            end if;
-          elsif ex = "10" then
-            if sign='0' then
-                  data_out(15 downto 0) <= memory(to_integer(unsiged(adresse))(15 downto 0);
-            else
-                  data_out(31)<= memory(to_integer(unsiged(adresse))(15);
-                  data_out(14 downto 0) <= memory(to_integer(unsiged(adresse))(14 downto 0);
-            end if;
+  -- purpose: gere les accès mémoire
+  -- type   : combinational
+  -- inputs : read_write,data_in,sel_signex
+  -- outputs: data_out
+  comb          : process (read_write, data_in, ex, sign, mem_access) is
+  begin  -- process comb
+    if mem_access = '1' then
+      data_out <= (others => '0');
+      if read_write = '0' then          -- lecture
+        if size = "01" then
+          if sign = '0' then
+            data_out(7 downto 0) <= memory(to_integer(unsiged(adresse)))(7 downto 0);
           else
-            data_out<=memory(to_integer(unsiged(adresse));           
+            data_out(31)         <= memory(to_integer(unsiged(adresse)))(7);
+            data_out(6 downto 0) <= memory(to_integer(unsiged(adresse)))(6 downto 0);
           end if;
-
-
-
-          else --ecriture
-            if ex = "01" then
-              if sign='0' then
-                  memory(to_integer(unsiged(adresse))(7 downto 0) <= data_in(7 downto 0) ;
-            else
-                  memory(to_integer(unsiged(adresse))(31) <= data_in(7) ;
-                  memory(to_integer(unsiged(adresse))(6 downto 0) <= data_in(6 downto 0) ;
-            end if;
-          elsif ex = "10" then
-            if sign='0' then
-                  memory(to_integer(unsiged(adresse))(15 downto 0) <= data_in(15 downto 0) ;
-            else
-                  memory(to_integer(unsiged(adresse))(31) <= data_in(15) ;
-                  memory(to_integer(unsiged(adresse))(14 downto 0) <= data_in(14 downto 0) ;
-            end if;
+        elsif size = "10" then
+          if sign = '0' then
+            data_out(15 downto 0) <= memory(to_integer(unsiged(adresse)))(15 downto 0);
           else
-                  memory(to_integer(unsiged(adresse)) <= data_in ;
+            data_out(31)          <= memory(to_integer(unsiged(adresse)))(15);
+            data_out(14 downto 0) <= memory(to_integer(unsiged(adresse)))(14 downto 0);
           end if;
-                    
+        else
+          data_out <= memory(to_integer(unsiged(adresse)));
         end if;
-      end process comb;
 
-    end architecture A;
+
+
+      else                              --ecriture
+        if size = "01" then
+          memory(to_integer(unsiged(adresse)))(7 downto 0) <= data_in(7 downto 0);
+        elsif size = "10" then
+          memory(to_integer(unsiged(adresse)))(15 downto 0) <= data_in(15 downto 0);
+        else
+          memory(to_integer(unsiged(adresse))) <= data_in;
+        end if;
+      end if;
+
+
+
+    else
+      data_out <= data_out;
+    end if;
+  end process comb;
+
+end architecture A;
